86d821aa3262c5c46fd879c9400374431a315a8f
Altamash-Azam <azamaltamash007@gmail.com>
2025-07-26 09:12:19 +0530

    auth files created

---
+++ src/auth.ts
@@ -0,0 +1,63 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import userModel from "./models/User.model";
+import connectDB from "./lib/mongodb";
+
+export const {handlers , signIn, signOut, auth} = NextAuth({
+    session: {strategy:'jwt'},
+    providers:[
+        Credentials({
+            credentials: {
+                email: {label: "Email", type:"email"},
+                password: {label:"Password", type:"password"}
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password){
+                    return null;
+                }
+
+                await connectDB();
+
+                const user = await userModel.findOne({email: credentials.email});
+
+                if(!user || !user.hashedPassword){
+                    return null;
+                }
+
+                const isPasswordCorrect = await bcrypt.compare(
+                    credentials.password as string,
+                    user.hashedPassword
+                )
+
+                if(!isPasswordCorrect){
+                    return null;
+                }
+
+                return {
+                    id: user._id.toString(),
+                    username: user.username,
+                    email: user.email
+                }
+            }
+        })
+    ],
+    callbacks: {
+
+        async jwt({token, user}) {
+            if(user){
+                token.id = user.id as string;
+                token.username = user.username;
+            }
+            return token;
+        },
+
+        async session({session, token}) {
+            if(session.user){
+                session.user.username = token.username;
+                session.user.id = token.id;
+            }
+            return session;
+        }
+    }
+});
